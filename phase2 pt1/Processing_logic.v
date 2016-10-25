e98d63a9ff09c7e0be28e634db0cb404b54f6b9d
Najib Hourani <nhourani@usc.edu>
2016-10-25 19:42:16 +0200

    Added waveform script for SimVision. FIFOs need write enable!

--- phase2 pt1/Processing_logic.v
+++ phase2 pt1/Processing_logic.v
@@ -67,14 +67,14 @@ module Processing_logic(
              PRECHARGE    = 4'd15;
    
    //OPCODES taken from tb.v
-   parameter CMD_NOP  = 3'd0,   // 000: No Operation (NOP)  
-             CMD_SCR  = 3'd1,   // 001: Scalar Read  (SCR)
-             CMD_SCW  = 3'd2,   // 010: Scalar Write  (SCW)
-             CMD_BLR  = 3'd3,   // 011: Block Read (BLR)
-             CMD_BLW  = 3'd4,   // 100: Block Write ((BLW)
-             CMD_ATR  = 3'd5,   // 101: Atomic Read (ATR)
-             CMD_ATW  = 3'd6,   // 110: Atomic Write (ATW)
-             CMD_NOP2 = 3'd7;   // 111: No Operation (NOP)
+   parameter CMD_NOP  = 'd0,   // 000: No Operation (NOP)  
+             CMD_SCR  = 'd1,   // 001: Scalar Read  (SCR)
+             CMD_SCW  = 'd2,   // 010: Scalar Write  (SCW)
+             CMD_BLR  = 'd3,   // 011: Block Read (BLR)
+             CMD_BLW  = 'd4,   // 100: Block Write ((BLW)
+             CMD_ATR  = 'd5,   // 101: Atomic Read (ATR)
+             CMD_ATW  = 'd6,   // 110: Atomic Write (ATW)
+             CMD_NOP2 = 'd7;   // 111: No Operation (NOP)
    
    input 	 clk, ck, reset, ready;
    input 	 CMD_empty, RETURN_full;
@@ -86,7 +86,7 @@ module Processing_logic(
    output reg CMD_get;
    output reg DATA_get;
    output reg RETURN_put;
-   output reg  [25:0] RETURN_address;
+   output reg [25:0] RETURN_address;
    output reg [15:0] RETURN_data;
    output reg cs_bar;
    output reg ras_bar;
@@ -105,18 +105,25 @@ module Processing_logic(
    reg DM_flag;
    integer i,j;
    reg [2:0] Pointer;
-   //reg read;
-   //reg write;
-  wire [1:0] sz;
-  wire [ 2:0] op;
-  wire [2:0] cmd;
-  wire [25:0] addr;
-  wire [15:0] read_data;
+   wire [1:0]  sz;
+   wire [2:0]  op;
+   wire [2:0]  cmd;
+   wire [25:0] addr;
+   wire [15:0] read_data;
+   wire [12:0] row_addr;
+   wire [9:0]  col_addr;
+   wire [2:0]  bank_addr;
   //component instantiation
   ddr3_ring_buffer8 ring_buffer(read_data, listen, DQS_in[0], Pointer[2:0], DQ_in, reset);
 
 //----------------------Real Code Begins-----------------//
-assign {cmd, sz, op, addr} = CMD_data_out;
+assign cmd       = CMD_data_out[33:31];
+assign sz        = CMD_data_out[30:29]; 
+assign op        = CMD_data_out[28:26];
+assign addr      = CMD_data_out[25:0];
+assign bank_addr = addr[25:23];
+assign row_addr  = addr[22:10]; 
+assign col_addr  = addr[9:0];
 
 //FSM State Memory, NSL, OFL
 always @(posedge clk) 
@@ -139,8 +146,6 @@ begin
     DQS_out        <= 2'b10;
     ts_con         <= 0;
     listen         <= 0;
-    //read           <= 0;
-    //write          <= 0;
   end
   else
   begin
@@ -148,30 +153,31 @@ begin
     CMD_get    <= 1'b0;
     DATA_get   <= 1'b0;
     RETURN_put <= 1'b0;
-	DQS_out <= ~ DQS_out ;
+    DQS_out <= ~ DQS_out ;
+
     case (state)
       IDLE        : begin
                       listen <= 0;
                       {cs_bar, ras_bar, cas_bar, we_bar} <= 4'b0111; //is this idle???
                       if (ready==1)  //initialization complete 
                       begin 
-                        if (CMD_empty==1) //active-low!
+                        /*if (CMD_empty==1) //Connected to empty_bar: 0 means empty!
                           state <= IDLE;
-                        else if( RETURN_full == 0)
-						  begin
-                          CMD_get <= 1'b1; 
+                        else if(RETURN_full == 0)
+		          begin
+                          CMD_get <= 1'b1; */
                           state   <= DECODE;
-						  i <= 0;
-						  j <= 0;
-                          end
+			  i <= 0;
+			  j <= 0;
+                        //  end
                       end
                     end
                       
       DECODE      : begin
                     //Phase 2 pt.1 only supports NOP, ACT, SCR, SCW
-					CMD_get <= 0;
-                    case(cmd) //What bits dictate the command???
+	     	    CMD_get <= 0;
 
+                    case(CMD_data_out[33:31]) 
                       CMD_NOP  : begin
                                    state <= IDLE;
                                    {cs_bar, ras_bar, cas_bar, we_bar} <= 4'b0111;
@@ -199,49 +205,57 @@ begin
       PRECHARGE_PD: begin end 
       MRS_MPR     : begin end 
       ZQ_CAL      : begin end 
+
       ACTIVATE    : begin 
-                 
-						state <= BANK_ACTIVE;
-						{cs_bar, ras_bar, cas_bar, we_bar} <= 4'b0011;
+	       	      state    <= BANK_ACTIVE;
+                      BA       <= bank_addr;
+                      A[22:10] <= row_addr; 
+                      i <= i+1;
+                      j <= j+1; //timing counter starts now.
+		      {cs_bar, ras_bar, cas_bar, we_bar} <= 4'b0011;
                     end 
+
       BANK_ACTIVE : begin
-					 
-					 BA <= addr[25:23];
-					 A <= addr[22:10];
-					 if(cmd == CMD_SCR && i == tRCD)
-					 begin
-						state <= READ;
-						{cs_bar, ras_bar, cas_bar, we_bar} <=4'b0101 ;
-						i <= 0;
-					end
-					 if(cmd == CMD_SCW && i == tRCD)
-					 begin
-						state <= WRITE;
-						{cs_bar, ras_bar, cas_bar, we_bar} <= 4'b0100;
-						i <= 0;
-					end
-					 i <= i+ 1;
-					end 
+                    BA <= 'bz; //tri-state util tRCD
+                    A  <= 'bz; //tri-state until tRCD
+                      if(cmd == CMD_SCR && i == tRCD-1)
+                      begin
+                        state  <= READ;
+                        BA     <= bank_addr;
+                        A[9:0] <= col_addr; //send column address
+                        {cs_bar, ras_bar, cas_bar, we_bar} <=4'b0101 ;
+                        i <= 0;
+                      end
+                      if(cmd == CMD_SCW && i == tRCD-1) //counter goes from 0 to tRCD-1 (total of tRCD cycles)
+                      begin
+                        state <= WRITE;
+                        BA     <= bank_addr;
+                        A[9:0] <= col_addr; //send column address
+                        {cs_bar, ras_bar, cas_bar, we_bar} <= 4'b0100;
+                        i <= 0;
+                      end
+                      i <= i+ 1;
+                      end
+ 
       ACTIVE_PD   : begin end 
       READ        : begin
-						if(j == 0)
-						begin
-							
-							BA <= addr[25:23];
-							A <= addr[9:0];
-							//CMD_get <= 1;
-							i <= i+1;
-						end
-						if(i == RL -1)
-							RETURN_put <= 1'b1;
-						if(i == RL)
-						begin
-							RETURN_data <= {addr, read_data};
-							RETURN_put <= 0;
-							state <= IDLE;
-							i <= 0;
-						end
-					   j <= j+1;
+	              if(j == 0)
+	              begin
+	              	BA <= addr[25:23];
+	              	A  <= addr[9:0];
+	              	//CMD_get <= 1;
+	              	i <= i+1;
+	              end
+	              if(i == RL -1)
+	              	RETURN_put <= 1'b1;
+	              if(i == RL)
+	              begin
+	              	RETURN_data <= {addr, read_data};
+	              	RETURN_put <= 0;
+	              	state <= IDLE;
+	              	i <= 0;
+	              end
+                      j <= j+1;
 					//  CMD_get <= 0;
 					//  if( j == tRRD)
 					//  begin
@@ -256,20 +270,20 @@ begin
 					//		RETURN_data <= {addr, DQ_in};
 					//		k <= 0;
 					//		j <= 0;
-						end
-						
+			end
+				
                     
       WRITE       : begin 
-                  
-						BA <= addr[25:23];
-						A <= addr[9:0];
-						i <= i+1;
-						if(i == WL)
-						begin
-							state <= IDLE;
-							i <= 0;
-						end
+       		      BA <= addr[25:23];
+       		      A  <= addr[9:0];
+       		      i  <= i+1;
+       		      if (i == WL)
+       		      begin
+       			state <= IDLE;
+       			i <= 0;
+       		      end
                     end 
+
       READ_AP     : begin end 
       WRITE_AP    : begin end 
       PRECHARGE   : begin end 
@@ -292,10 +306,10 @@ end
 
 always @(negedge clk)
   begin
-    
-    if((state == WRITE) && (i==WL -1 ))
-		DQ_out <= DATA_data_out;
+    if((state == WRITE) && (i==WL -1 )) begin
+	DQ_out <= DATA_data_out;
         DM <= 2'b00;
+    end
     else
         DM <= 2'b11;	
   end
